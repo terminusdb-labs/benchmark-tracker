d5fd94fa6b4b5e0a227807775d30fca1e6a4c44f
Sean Leather <sean@terminusdb.com>
2021-08-03 13:33:55 +0200

    First release

---
+++ src/git.js
@@ -0,0 +1,97 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pull = exports.push = exports.cmd = void 0;
+const exec_1 = require("@actions/exec");
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+async function capture(cmd, args) {
+    const res = {
+        stdout: '',
+        stderr: '',
+        code: null,
+    };
+    try {
+        const code = await exec_1.exec(cmd, args, {
+            listeners: {
+                stdout(data) {
+                    res.stdout += data.toString();
+                },
+                stderr(data) {
+                    res.stderr += data.toString();
+                },
+            },
+        });
+        res.code = code;
+        return res;
+    }
+    catch (err) {
+        const msg = `Command '${cmd}' failed with args '${args.join(' ')}': ${res.stderr}: ${err}`;
+        core.debug(`@actions/exec.exec() threw an error: ${msg}`);
+        throw new Error(msg);
+    }
+}
+async function cmd(...args) {
+    core.debug(`Executing Git: ${args.join(' ')}`);
+    const userArgs = [
+        '-c',
+        'user.name=github-action-benchmark',
+        '-c',
+        'user.email=github@users.noreply.github.com',
+        '-c',
+        'http.https://github.com/.extraheader=', // This config is necessary to support actions/checkout@v2 (#9)
+    ];
+    const res = await capture('git', userArgs.concat(args));
+    if (res.code !== 0) {
+        throw new Error(`Command 'git ${args.join(' ')}' failed: ${JSON.stringify(res)}`);
+    }
+    return res.stdout;
+}
+exports.cmd = cmd;
+function getRemoteUrl(token) {
+    var _a;
+    const fullName = (_a = github.context.payload.repository) === null || _a === void 0 ? void 0 : _a.full_name;
+    if (!fullName) {
+        throw new Error(`Repository info is not available in payload: ${JSON.stringify(github.context.payload)}`);
+    }
+    return `https://x-access-token:${token}@github.com/${fullName}.git`;
+}
+async function push(token, branch, ...options) {
+    core.debug(`Executing 'git push' to branch '${branch}' with token and options '${options.join(' ')}'`);
+    const remote = getRemoteUrl(token);
+    let args = ['push', remote, `${branch}:${branch}`, '--no-verify'];
+    if (options.length > 0) {
+        args = args.concat(options);
+    }
+    return cmd(...args);
+}
+exports.push = push;
+async function pull(token, branch, ...options) {
+    core.debug(`Executing 'git pull' to branch '${branch}' with token and options '${options.join(' ')}'`);
+    const remote = token !== undefined ? getRemoteUrl(token) : 'origin';
+    let args = ['pull', remote, branch];
+    if (options.length > 0) {
+        args = args.concat(options);
+    }
+    return cmd(...args);
+}
+exports.pull = pull;
+//# sourceMappingURL=git.js.map
