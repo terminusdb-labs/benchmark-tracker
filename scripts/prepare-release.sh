7e3b4ba529635da189c1ba0e35e76863d3b2d543
Sean Leather <sean@terminusdb.com>
2021-08-03 09:53:24 +0200

    git rm -rf --ignore-unmatch node_modules
    Use --ignore-unmatch in case node_modules isn't checked in.

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -47,7 +47,7 @@ cp -R node_modules .release/node_modules
 
 git checkout "$version"
 git pull
-git rm -rf node_modules
+git rm -rf --ignore-unmatch node_modules
 rm -rf node_modules  # remove node_modules/.cache
 mkdir -p src
 
